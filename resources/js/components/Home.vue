c56b5566b53e3e4a4f553c600e6e61e131c21436
norwel <norwel1217@gmail.com>
2023-08-12 10:56:12 +0800

    first commit

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-card>
+        <v-card-title style="background-color:#B2DFDB">Home</v-card-title>
+        <v-card-text>
+            <p>some transcations & logics here....</p>
+        </v-card-text>
+    </v-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
